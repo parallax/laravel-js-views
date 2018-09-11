fd1fe0c6b7d64a1541750c35cf7bce0e8cc98765
Brad Cornes <bradlc41@gmail.com>
2018-09-11 20:54:51 +0100

    preact: allow render function to return a promise

--- src/js/preact/entry-server.js
+++ src/js/preact/entry-server.js
@@ -5,5 +5,9 @@ import(/* webpackChunkName: 'js/server/[request]' */ `__laravel_views__/${
   global.__INITIAL_LARAVEL_PAGE__
 }.js`).then(c => {
   let Component = c.default
-  print(JSON.stringify(render(Component, global.__INITIAL_LARAVEL_PROPS__)))
+  Promise.resolve(render(Component, global.__INITIAL_LARAVEL_PROPS__)).then(
+    res => {
+      print(JSON.stringify(res))
+    }
+  )
 })
