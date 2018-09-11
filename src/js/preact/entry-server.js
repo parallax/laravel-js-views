0206b1df8f9c460c8509e5090b96133e498e7378
Brad Cornes <bradlc41@gmail.com>
2018-09-11 21:04:18 +0100

    tidy up

--- src/js/preact/entry-server.js
+++ src/js/preact/entry-server.js
@@ -1,13 +1,11 @@
-import { h } from 'preact'
 import render from '__laravel_render_server__'
 
 import(/* webpackChunkName: 'js/server/[request]' */ `__laravel_views__/${
   global.__INITIAL_LARAVEL_PAGE__
-}.js`).then(c => {
-  let Component = c.default
-  Promise.resolve(render(Component, global.__INITIAL_LARAVEL_PROPS__)).then(
-    res => {
-      print(JSON.stringify(res))
-    }
-  )
+}.js`).then(component => {
+  Promise.resolve(
+    render(component.default || component, global.__INITIAL_LARAVEL_PROPS__)
+  ).then(res => {
+    print(JSON.stringify(res))
+  })
 })
--- src/js/vue/entry-server.js
+++ src/js/vue/entry-server.js
@@ -2,11 +2,10 @@ import render from '__laravel_render_server__'
 
 import(/* webpackChunkName: 'js/server/[request]' */ `__laravel_views__/${
   global.__INITIAL_LARAVEL_PAGE__
-}.vue`).then(c => {
-  let Component = c.default || c
-  Promise.resolve(render(Component, global.__INITIAL_LARAVEL_PROPS__)).then(
-    res => {
-      print(JSON.stringify(res))
-    }
-  )
+}.vue`).then(component => {
+  Promise.resolve(
+    render(component.default || component, global.__INITIAL_LARAVEL_PROPS__)
+  ).then(res => {
+    print(JSON.stringify(res))
+  })
 })
