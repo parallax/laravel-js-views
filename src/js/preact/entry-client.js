b4ef3dc585d94a7a4d425ea8cb058472e7cd5b46
Brad Cornes <bradlc41@gmail.com>
2018-10-21 15:56:03 +0100

    remove client-side routing

--- src/js/preact/entry-client.js
+++ src/js/preact/entry-client.js
@@ -1,80 +1,13 @@
 import render from '__laravel_render_client__'
 
-function getCurrentUrl() {
-  return `${window.location.pathname || ''}${window.location.search || ''}`
-}
-
-let loaded = {
-  [getCurrentUrl()]: import(/* webpackChunkName: 'js/[request]' */ `__laravel_views__/${
-    window.Laravel.___page
-  }.js`).then(c => {
-    return {
-      view: window.Laravel.___page,
-      Component: c.default,
-      data: window.Laravel.___props
-    }
-  })
-}
-
-function main({ Component, data, view }) {
-  render(Component, data)
-}
-
-loaded[getCurrentUrl()].then(main)
-
-window.addEventListener('click', e => {
-  let link = e.target.closest('a')
-  if (!link) return
-  e.preventDefault()
-  let path = link.getAttribute('href')
-  routeTo(path)
+import(/* webpackChunkName: 'js/[request]' */ `__laravel_views__/${
+  window.Laravel.___page
+}.js`).then(c => {
+  render(c.default || c, window.Laravel.___props)
 })
-
-window.addEventListener('popstate', () => {
-  routeTo(getCurrentUrl(), false)
-})
-
-// window.addEventListener('mouseover', e => {
-//   let link = e.target.closest('a')
-//   if (!link) return
-//   load(link.getAttribute('href'))
-// })
-
-function routeTo(path, push = true) {
-  load(path).then(stuff => {
-    push && history.pushState({}, '', path)
-    main(stuff)
-  })
-}
-
-function load(path) {
-  if (!loaded[path]) {
-    let v
-    let d
-    loaded[path] = window
-      .fetch(path, { headers: { 'x-requested-with': 'XMLHttpRequest' } })
-      .then(res => res.json())
-      .then(json => {
-        v = json.view
-        d = json.props
-
-        return import(/* webpackChunkName: 'js/client/pages/[request]' */ `__laravel_views__/${
-          json.view
-        }.js`)
-      })
-      .then(c => {
-        return {
-          Component: c.default,
-          view: v,
-          data: d
-        }
-      })
-  }
-  return loaded[path]
-}
 
 if (module.hot) {
   module.hot.accept()
-  let NextApp = require(`__laravel_views__/${view}.js`).default
-  render(NextApp, data)
+  let NextApp = require(`__laravel_views__/${window.Laravel.___page}.js`)
+  render(NextApp.default || NextApp, window.Laravel.___props)
 }
--- src/js/vue/entry-client.js
+++ src/js/vue/entry-client.js
@@ -1,80 +1,13 @@
 import render from '__laravel_render_client__'
 
-function getCurrentUrl() {
-  return `${window.location.pathname || ''}${window.location.search || ''}`
-}
-
-let loaded = {
-  [getCurrentUrl()]: import(/* webpackChunkName: 'js/[request]' */ `__laravel_views__/${
-    window.Laravel.___page
-  }.vue`).then(c => {
-    return {
-      view: window.Laravel.___page,
-      Component: c.default || c,
-      data: window.Laravel.___props
-    }
-  })
-}
-
-function main({ Component, data, view }) {
-  render(Component, data)
-}
-
-loaded[getCurrentUrl()].then(main)
-
-window.addEventListener('click', e => {
-  let link = e.target.closest('a')
-  if (!link) return
-  e.preventDefault()
-  let path = link.getAttribute('href')
-  routeTo(path)
+import(/* webpackChunkName: 'js/[request]' */ `__laravel_views__/${
+  window.Laravel.___page
+}.vue`).then(c => {
+  render(c.default || c, window.Laravel.___props)
 })
-
-window.addEventListener('popstate', () => {
-  routeTo(getCurrentUrl(), false)
-})
-
-// window.addEventListener('mouseover', e => {
-//   let link = e.target.closest('a')
-//   if (!link) return
-//   load(link.getAttribute('href'))
-// })
-
-function routeTo(path, push = true) {
-  load(path).then(stuff => {
-    push && history.pushState({}, '', path)
-    main(stuff)
-  })
-}
-
-function load(path) {
-  if (!loaded[path]) {
-    let v
-    let d
-    loaded[path] = window
-      .fetch(path, { headers: { 'x-requested-with': 'XMLHttpRequest' } })
-      .then(res => res.json())
-      .then(json => {
-        v = json.view
-        d = json.props
-
-        return import(/* webpackChunkName: 'js/client/pages/[request]' */ `__laravel_views__/${
-          json.view
-        }.vue`)
-      })
-      .then(c => {
-        return {
-          Component: c.default || c,
-          view: v,
-          data: d
-        }
-      })
-  }
-  return loaded[path]
-}
 
 if (module.hot) {
   module.hot.accept()
-  let NextApp = require(`__laravel_views__/${view}.vue`)
-  render(NextApp.default || NextApp, data)
+  let NextApp = require(`__laravel_views__/${window.Laravel.___page}.vue`)
+  render(NextApp.default || NextApp, window.Laravel.___props)
 }
